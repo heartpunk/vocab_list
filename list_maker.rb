8b2c38b530d97f8ad32fc62a580f92325e8600df
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2013-07-15 23:46:04 -0700

    some improvements to the vocab list class and the preprocessing.

--- list_maker.rb
+++ list_maker.rb
@@ -1,4 +1,22 @@
 require './vocab_list'
-require 'pp'
+require 'set'
 
-pp VocabList.new(ARGF.read.split(/\s/).reject(&:empty?)).sorted_list
+PUNCTUATION = "[\.,':;?!-]"
+
+known_words_path = "/Users/tehgeekmeister/.known_words"
+known_words_list = File.file?(known_words_path) ? open(known_words_path).read.lines.map(&:strip) : []
+known_words = Set.new known_words_list
+
+def remove_byte_order_markers str
+	str.gsub("\xEF\xBB\xBF".force_encoding("UTF-8"), '')
+end
+
+def remove_leading_or_trailing_punctuation str
+  str.gsub(/\A#{PUNCTUATION}*/, '').gsub(/#{PUNCTUATION}*\z/, '')
+end
+
+text = ARGF.read
+text = remove_byte_order_markers text
+text = text.split(/\s|--/).map(&:strip).map(&:downcase).map {|w| remove_leading_or_trailing_punctuation w}.reject(&:empty?)
+
+puts VocabList.new(text, known_words).output
--- vocab_list.rb
+++ vocab_list.rb
@@ -1,22 +1,22 @@
 require 'set'
 
 class VocabList
-  attr_accessor :sorted_list, :words
+  attr_accessor :sorted_list, :words, :known_words
 
-  def initialize(list)
+  def initialize(list, known_words = Set.new)
     self.sorted_list = []
     self.words = Set.new
+    self.known_words = known_words
     list.each {|word| add_word word}
   end
 
   def add_word word
-    unless words.include? word
-      words.add word
-      sorted_list << word
-    end
+    return if words.include?(word) or known_words.include?(word)
+    words.add word
+    sorted_list << word
   end
 
-  def to_str
-    sorted_list.to_str
+  def output
+    sorted_list.map {|word| word + "\n"}.join('')
   end
 end
